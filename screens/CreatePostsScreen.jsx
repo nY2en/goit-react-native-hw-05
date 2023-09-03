2e08e624192ae34fb14694a701c9471cd33b3fb1
Nikita <chebanikita@icloud.com>
2023-09-04 01:08:40 +0300

    sth

--- screens/CreatePostsScreen.jsx
+++ screens/CreatePostsScreen.jsx
@@ -1,9 +1,108 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
+import { AntDesign, FontAwesome5 } from "@expo/vector-icons";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+  Image,
+} from "react-native";
+
 const CreatePostsScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+
+    setPhoto(photo.uri);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.navigate("Posts")}>
+            <AntDesign name="arrowleft" size={32} color="#BDBDBD" />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Створити публікацію</Text>
+        </View>
+        <View style={styles.main}>
+          <Camera style={styles.camera} ref={setCamera}>
+            {photo && (
+              <View style={styles.tookedPhoto}>
+                <Image soure={{ uri: photo }} />
+              </View>
+            )}
+            <TouchableOpacity style={styles.icon} onPress={takePhoto}>
+              <FontAwesome5 name="camera" size={24} color="#BDBDBD" />
+            </TouchableOpacity>
+          </Camera>
+          <Formik
+            initialValues={{ name: "", location: "" }}
+            onSubmit={(values, { resetForm }) => {
+              const newPost = {
+                ...values,
+                photo,
+              };
+              console.log(newPost);
+
+              navigation.navigate("Posts", newPost);
+              resetForm();
+            }}
+          >
+            {({ values, handleChange, handleSubmit }) => (
+              <View>
+                <TextInput
+                  name="name"
+                  value={values.name}
+                  onChangeText={handleChange("name")}
+                  placeholder="Назва..."
+                  style={{ ...styles.input, marginBottom: 16 }}
+                ></TextInput>
+                <TextInput
+                  name="location"
+                  value={values.location}
+                  onChangeText={handleChange("location")}
+                  placeholder="Місцевість..."
+                  style={{ ...styles.input, marginBottom: 32 }}
+                ></TextInput>
+
+                <TouchableOpacity
+                  style={styles.button}
+                  activeOpacity={0.8}
+                  onPress={handleSubmit}
+                >
+                  <Text style={styles.buttonTitle}>Опублікувати</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          </Formik>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -12,7 +111,80 @@ export default CreatePostsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+    paddingTop: 36,
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#BDBDBD",
+  },
+
+  headerTitle: {
+    marginLeft: 64,
+
+    fontFamily: "Roboto500",
+    fontSize: 17,
+  },
+
+  main: {
+    position: "relative",
+    paddingHorizontal: 16,
+  },
+
+  camera: {
+    height: 240,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 60,
+  },
+
+  icon: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  tookedPhoto: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: 240,
+    width: "100%",
+    // pointerEvents: "none",
+  },
+
+  input: {
+    paddingVertical: 16,
+
+    fontFamily: "Roboto400",
+    fontSize: 16,
+
+    borderBottomWidth: 1,
+    borderBottomColor: "#E8E8E8",
+  },
+
+  button: {
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+
+    borderRadius: 100,
+
+    backgroundColor: "#FF6C00",
+  },
+
+  buttonTitle: {
+    fontFamily: "Roboto400",
+    fontSize: 16,
+    textAlign: "center",
+
+    color: "#FFF",
   },
 });
--- screens/PostsScreen.jsx
+++ screens/PostsScreen.jsx
@@ -1,34 +1,76 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import {
+  MaterialIcons,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
-const PostsScreen = () => {
+const PostsScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          alignItems: "flex-end",
-          paddingTop: 42,
-          paddingBottom: 16,
-          paddingHorizontal: 8,
-          borderBottomWidth: 1,
-          borderBottomColor: "#BDBDBD",
-        }}
-      >
+      <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
           <MaterialIcons name="logout" size={32} color="#BDBDBD" />
         </TouchableOpacity>
       </View>
 
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Text>PostsScreen</Text>
+      <View style={styles.main}>
+        <View style={styles.user}>
+          <Image style={styles.userPhoto} />
+          <View style={{ marginLeft: 8 }}>
+            <Text style={styles.userName}>User Name</Text>
+            <Text style={styles.userEmail}>User Email</Text>
+          </View>
+        </View>
+
+        <FlatList
+          data={posts}
+          keyExtractor={(item, indx) => indx.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Image source={{ uri: item.photo }} style={styles.cardPhoto} />
+              <Text style={styles.cardName}> {item.name}</Text>
+              <View style={styles.cardInfo}>
+                <TouchableOpacity>
+                  <FontAwesome
+                    name="comment-o"
+                    size={24}
+                    color="#BDBDBD"
+                    style={styles.cardIcon}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <View style={styles.cardInfo}>
+                    <MaterialCommunityIcons
+                      name="map-marker-outline"
+                      size={24}
+                      color="#BDBDBD"
+                    />
+                    <Text style={styles.cardLocation}> {item.location}</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -39,5 +81,82 @@ export default PostsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  header: {
+    alignItems: "flex-end",
+    marginBottom: 32,
+    paddingTop: 36,
+    paddingBottom: 16,
+    paddingHorizontal: 8,
+
+    borderBottomWidth: 1,
+    borderBottomColor: "#BDBDBD",
+  },
+
+  main: {
+    paddingHorizontal: 16,
+
+    paddingBottom: 200,
+  },
+
+  user: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+
+  userName: {
+    fontFamily: "Roboto500",
+    fontSize: 13,
+
+    color: "#212121",
+  },
+
+  userEmail: {
+    fontFamily: "Roboto400",
+    fontSize: 11,
+    color: "rgba(33, 33, 33, 0.8)",
+  },
+  userPhoto: {
+    width: 60,
+    height: 60,
+    backgroundColor: "pink",
+    borderRadius: 25,
+  },
+
+  card: {
+    marginBottom: 32,
+  },
+
+  cardPhoto: {
+    height: 240,
+    marginBottom: 8,
+    borderRadius: 10,
+  },
+
+  cardName: {
+    marginBottom: 8,
+
+    fontFamily: "Roboto500",
+    fontSize: 16,
+    color: "#212121",
+  },
+
+  cardInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  cardIcon: {
+    marginLeft: 8,
+  },
+
+  cardLocation: {
+    fontFamily: "Roboto500",
+    fontSize: 16,
+    color: "#212121",
+    textDecorationLine: "underline",
   },
 });
